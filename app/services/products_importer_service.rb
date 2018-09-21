4b1418c4937c33e504a8127774f0050318391afc
Wojtek Kostanski <wojtek@tappest.com>
2018-09-21 16:49:22 +0200

    add slug duplication fix

--- app/services/products_importer_service.rb
+++ app/services/products_importer_service.rb
@@ -6,21 +6,28 @@ class ProductsImporterService
   end
 
   def import
-    p "start import"
     CSV.foreach(@file.path, headers: true, col_sep: ';') do |row|
       unless row['name'].nil?
-        @product = Spree::Product.create!(name: row['name'], description: row['description'], slug: row['slug'], available_on: row['availability_date'], price: row['price'], shipping_category_id: 1)
+        p row['slug']
+        p Spree::Product.find_by_slug(row['slug']).nil?
+        if Spree::Product.find_by_slug(row['slug']).nil?
+          slug = row['slug']
+        else
+          unless Spree::Product.find_by_slug(slug).nil?
+            slug = "#{row['slug']}#{row['slug'][-1]}"
+          end
+        end
+        @product = Spree::Product.create!(name: row['name'], description: row['description'], slug: slug, available_on: row['availability_date'], price: row['price'], shipping_category_id: 1)
       end
-      p "row taxon nil? ===> #{row['category'].nil?}"
 
       unless row['category'].nil?
         @taxon = Spree::Taxon.find_or_create_by!(name: row['category'])
       end
-      p @taxon
-      p @product
       unless @taxon.nil? && @product.nil?
-      Spree:Products_taxon.create!(product_id: @product.id, taxon_id: @taxon.id)
+      # TODO: How connect taxon and product
+      # Spree:Products_taxon.create!(product_id: @product.id, taxon_id: @taxon.id)
       end
+      # TODO: How add product quantity
     end
   end
 end
