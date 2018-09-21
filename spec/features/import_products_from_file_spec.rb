fe59cbc6c20f5b063baef7cbd6ff4ad224fed032
Wojtek Kostanski <wojtek@tappest.com>
2018-09-21 18:34:12 +0200

    Add test for file upload

--- spec/features/import_products_from_file_spec.rb
+++ spec/features/import_products_from_file_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 
-feature 'User create products from upload file' do
-  before :each do
-    @file = fixture_file_upload(Rails.root+'sample.csv', 'text/csv')
-  end
+feature 'Add products from CSV file' do
 
+  scenario 'upload should create products' do
+    expect(Spree::Product.count).to eq 0
+    visit 'admin/import'
+    attach_file('imported_file_data', Rails.root+'sample.csv')
+    click_button 'Import file'
+    expect(Spree::Product.count).to eq 3
+  end
 end
