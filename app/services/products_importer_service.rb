2e99eb8e90b9fa5e9d721ca4f5fd0b15e56b224f
Wojtek Kostanski <wojtek@tappest.com>
2018-09-21 07:11:37 +0200

    working on import

--- app/services/products_importer_service.rb
+++ app/services/products_importer_service.rb
@@ -11,10 +11,16 @@ class ProductsImporterService
       unless row['name'].nil?
         @product = Spree::Product.create!(name: row['name'], description: row['description'], slug: row['slug'], available_on: row['availability_date'], price: row['price'], shipping_category_id: 1)
       end
-      unless row['class'].nil?
-        @taxon = Spree::Taxon.find_or_create_by!(name: row['class'])
+      p "row taxon nil? ===> #{row['category'].nil?}"
+
+      unless row['category'].nil?
+        @taxon = Spree::Taxon.find_or_create_by!(name: row['category'])
       end
+      p @taxon
+      p @product
+      unless @taxon.nil? && @product.nil?
       Spree:Products_taxon.create!(product_id: @product.id, taxon_id: @taxon.id)
+      end
     end
   end
 end
